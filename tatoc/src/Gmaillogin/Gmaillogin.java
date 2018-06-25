cca990eef6f83cec2f76f672008e3cd2f6f574a5
Rahul Mohan <40262597+rahulmohan995@users.noreply.github.com>
2018-06-25 14:26:27 +0530

    Add files via upload

--- tatoc/src/Gmaillogin/Gmaillogin.java
+++ tatoc/src/Gmaillogin/Gmaillogin.java
@@ -1,19 +1,17 @@
 package Gmaillogin;
-
-import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions; 
+
+
 
 public class Gmaillogin {
-	
 	public void NumberOfUnreadEmails(WebDriver driver)
 	{
-		String Value=driver.findElement(By.cssSelector("#\\3a jh > div > div.aio.UKr6le > span > a")).getAttribute("aria-label");
+		String Value=driver.findElement(By.cssSelector(".J-Ke.n0")).getAttribute("aria-label");
 		String NumberOfUnreadMessages=Value.substring(6);
 		System.out.println(NumberOfUnreadMessages);
 	}
@@ -24,15 +22,21 @@ public class Gmaillogin {
 		Gmaillogin gmail= new Gmaillogin();
 		WebDriver driver= new ChromeDriver();
 		driver.get("https://accounts.google.com");
-		driver.findElement(By.id("identifierId")).sendKeys("rahulmohan995@gmail.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("identifierId")).sendKeys("sample96rajan");
 		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/content/span")).click();
 		driver.manage().timeouts().implicitlyWait(4,TimeUnit.SECONDS);
-		driver.findElement(By.cssSelector("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input")).sendKeys("Afrodox@789");
+		driver.findElement(By.cssSelector("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input")).sendKeys("Sample987*#");
 		driver.findElement(By.cssSelector("#passwordNext > content")).click();
+		driver.findElement(By.xpath("//a[@class='WaidBe']")).click();
 		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
 		gmail.NumberOfUnreadEmails(driver);
+		driver.close();
+		driver.quit();
 		
 		
 	}
 
 }
+
+
